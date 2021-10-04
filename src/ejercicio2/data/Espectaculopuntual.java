5c766c1ac6215477df92e254a1f9dd5940b46b18
i42piesr <i42piesr@uco.es>
2021-10-04 22:34:06 +0200

    modificado

--- src/ejercicio2/data/Espectaculopuntual.java
+++ src/ejercicio2/data/Espectaculopuntual.java
@@ -1,5 +1,39 @@
 package ejercicio2.data;
 
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+
 public class Espectaculopuntual extends Espectaculo {
 
+	
+	private LocalTime hora = LocalTime.now();
+	
+	private LocalDate fecha = LocalDate.now();
+
+	public Espectaculopuntual() {
+		
+	}
+
+	
+	public LocalTime getHora() {
+		return hora;
+	}
+	
+	public void setHora(LocalTime fecha) {
+		this.hora = fecha;
+		
+	}
+	
+	public LocalDate getFechaRepresentacion() {
+		return fecha;
+	}
+	
+	public void fechaRepresentacion(LocalDate fecha) {
+		this.fecha = fecha;
+	}
 }
+
+
