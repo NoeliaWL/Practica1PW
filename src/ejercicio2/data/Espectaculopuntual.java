d030c8b19cf6b6772cc97a8fdd0a400f34884e66
i42piesr <43422216+i42piesr@users.noreply.github.com>
2021-10-10 23:25:53 +0200

    Update Espectaculopuntual.java

--- src/ejercicio2/data/Espectaculopuntual.java
+++ src/ejercicio2/data/Espectaculopuntual.java
@@ -3,21 +3,35 @@ package ejercicio2.data;
 /**Clase Espectaculopuntual controla las sesiones y representaciones de 
 /*esta clase de espectaculos.
 /*
+/*@autores Noelia Hinojosa Sanchez, Rafael Piqueras Espinar
 */
 
 public class Espectaculopuntual extends Espectaculo {
 
 	Sesiones representacion;
-
+	
+	/**Este metodo que es el constructor inicia un 
+	/*array de sesiones.
+	/*
+	/*@param sesiones
+	*/
 	public Espectaculopuntual() {
 		representacion = new Sesiones();
 	}
-
+	
+	/**Este metodo devuelve una lista de sesiones
+	/*
+	/*@return representacion
+	*/
 	public Sesiones getRepresentacion() {
 		return representacion;
 
 	}
 
+	/**Este metodo asigna una lista de representaciones
+	/*
+	/*@param representacion
+	*/
 	public void setRepresentacion(Sesiones representacion) {
 		this.representacion = representacion;
 	}
