00a1e3521ecd0de674d9ddaefc730d55fab99ac4
i42piesr <43422216+i42piesr@users.noreply.github.com>
2021-10-10 23:18:07 +0200

    Update Espectaculopasemultiple.java

--- src/ejercicio2/data/Espectaculopasemultiple.java
+++ src/ejercicio2/data/Espectaculopasemultiple.java
@@ -2,10 +2,20 @@ package ejercicio2.data;
 
 import java.util.ArrayList;
 
+
+/**Esta clase 
+/*
+/*@authores Noelia Hinojosa Sanchez, Rafael Piqueras Espinar
+*/
 public class Espectaculopasemultiple extends Espectaculo {
 
 	ArrayList<Sesiones> pasemultiple;
-
+	
+	/**Este es el constructor de la clase
+	/*
+	/*
+	/*@param pasemultiple inicia un array de sesiones.
+	*/
 	public Espectaculopasemultiple() {
 		pasemultiple = new ArrayList<Sesiones>();
 	}
